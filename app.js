5969efc0cbda3610c60905138ec5242122af3a69
navodii <navodididilshara@gmail.com>
2024-11-20 19:01:58 +0530

    Completa operations

--- app.js
+++ app.js
@@ -3,9 +3,28 @@ function calc(){
   let num2
   num1 = Number(document.getElementById("num1").value)
   num2 = Number(document.getElementById("num2").value)
-  let output = document.getElementById("output")
 
-  output.innerHTML= (num1+num2)
+  let output = document.getElementById("output")
+  let operator = document.getElementById("op").value
+
+  switch (operator) {
+    case "+":
+      output.innerHTML = num1+num2;
+      break;
+
+    case "-":
+      output.innerHTML = num1-num2;
+      break;
+
+    case "*":
+      output.innerHTML = num1*num2;
+      break;
+
+    case "/":
+      output.innerHTML = num1/num2;
+      break;
+  }
+  
 }
 
 console.log("hi");
